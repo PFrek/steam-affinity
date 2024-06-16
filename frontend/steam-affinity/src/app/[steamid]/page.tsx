c5315e9b2a9ce97a0b86d01311b1ce5a32415930
PFrek <pedrogayaf@gmail.com>
2024-06-16 19:51:49 -0300

    Change routing to allow for different steamids

---
+++ frontend/steam-affinity/src/app/[steamid]/page.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import ProfileCard from "../components/ProfileCard/ProfileCard";
+import { getPlayerSummaries } from "../actions/actions";
+import FriendsList from "../components/FriendsList/FriendsList";
+import { PlayerSummary } from "../definitions/types";
+
+export default async function AffinityPage({ params }: { params: { steamid: string } }) {
+  const steamid = params.steamid
+
+  const resp = await getPlayerSummaries(steamid);
+
+  let summary: PlayerSummary | null = null
+  if (resp.length > 0) {
+    summary = resp[0];
+  }
+
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>Steam Affinity</h1>
+      </header>
+      <main className={styles.main}>
+        {summary != null ?
+          <>
+            <ProfileCard summary={summary!} affinity={null} affinityBoundaries={{ min: 0, max: 0 }} />
+            <FriendsList steamid={steamid} />
+          </>
+          :
+          <p className={styles.error}>Invalid id {steamid}</p>
+        }
+      </main>
+
+    </>
+  );
+}
--- frontend/steam-affinity/src/app/page.tsx
+++ frontend/steam-affinity/src/app/page.tsx
@@ -1,25 +1,13 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import ProfileCard from "./components/ProfileCard/ProfileCard";
-import { getFriendsList, getPlayerSummaries } from "./actions/actions";
-import FriendsList from "./components/FriendsList/FriendsList";
 
 export default async function Home() {
-  const steamid = "76561198081577408";
 
-  const resp = await getPlayerSummaries(steamid);
-  const summary = resp[0];
 
   return (
     <>
       <header className={styles.header}>
         <h1>Steam Affinity</h1>
       </header>
-      <main className={styles.main}>
-        <ProfileCard summary={summary} affinity={null} affinityBoundaries={{ min: 0, max: 0 }} />
-        <FriendsList steamid={steamid} />
-      </main>
-
     </>
   );
 }
