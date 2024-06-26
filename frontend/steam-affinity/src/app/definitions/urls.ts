5ab826fe693fbe4a2e95911152811999410950ca
PFrek <pedrogayaf@gmail.com>
2024-06-26 20:23:52 -0300

    Change url settings for prod

--- frontend/steam-affinity/src/app/definitions/urls.ts
+++ frontend/steam-affinity/src/app/definitions/urls.ts
@@ -1,1 +1,3 @@
-export const baseURL = "http://localhost:8080/api/v1"
+export const baseURL = process.env.NODE_ENV === "production"
+	? process.env.NEXT_PUBLIC_API_URL
+	: "http://localhost:8080/api/v1/";
