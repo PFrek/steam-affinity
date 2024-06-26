88b2155b18f63dcaa3f94a6ce476ff8687069e02
PFrek <pedrogayaf@gmail.com>
2024-06-26 12:34:29 -0300

    Add frontend Dockerfile

--- frontend/steam-affinity/src/app/page.tsx
+++ frontend/steam-affinity/src/app/page.tsx
@@ -32,8 +32,8 @@ export default function Home() {
         </div>
         <div className={styles.section}>
           <h2>What is Steam Affinity?</h2>
-          <p>Steam Affinity is a website project that allows comparing a user's Steam game library with those of their Steam friends.</p>
-          <p>The user's friends are ranked according to the similarity of their game libraries. Users with <em>similarly sized libraries</em> and with <em>many games in common</em> are ranked higher.</p>
+          <p>Steam Affinity is a website project that allows comparing a user&apos;s Steam game library with those of their Steam friends.</p>
+          <p>The user&apos;s friends are ranked according to the similarity of their game libraries. Users with <em>similarly sized libraries</em> and with <em>many games in common</em> are ranked higher.</p>
           <p>
             You can also see the games you both own, or the games that are missing from your library that the other user owns.
           </p>
@@ -41,13 +41,13 @@ export default function Home() {
         <div className={styles.section}>
           <h2>How to Use</h2>
           <p>
-            Enter your numeric Steam ID or that of a user you'd like to verify in the website URL.
+            Enter your numeric Steam ID or that of a user you&apos;d like to verify in the website URL.
           </p>
           <p>
             You can also use the form above to enter the numeric ID. Doing so and clicking the submit button will redirect you to the appropriate page.
           </p>
           <p>
-            You can also paste the user's profile URL, and the form should extract the numeric part automatically.
+            You can also paste the user&apos;s profile URL, and the form should extract the numeric part automatically.
           </p>
         </div>
         <div className={styles.section}>
@@ -59,7 +59,7 @@ export default function Home() {
             Affinity<sub>A,B</sub> = Similarity<sub>A,B</sub> ⋅ Weight<sub>A,B</sub>
           </p>
           <p>
-            The Similarity between two user's libraries is the number of matching games divided by the total number of games owned by both users, like so:
+            The Similarity between two user&apos;s libraries is the number of matching games divided by the total number of games owned by both users, like so:
           </p>
           <p className={styles.formula}>
             N<sub>A</sub> = number of games owned by user A
@@ -80,7 +80,7 @@ export default function Home() {
         <div className={styles.section}>
           <h2>Limitations</h2>
           <p>
-            Since this website uses the public Steam API to get the users' game libraries and friends lists, it is limited by the 100,000 daily calls they have set up.
+            Since this website uses the public Steam API to get the users&apos; game libraries and friends lists, it is limited by the 100,000 daily calls they have set up.
           </p>
           <p>
             Some effort was made to cache that information whenever possible to reduce the number of calls made, but it remains a possible issue.
